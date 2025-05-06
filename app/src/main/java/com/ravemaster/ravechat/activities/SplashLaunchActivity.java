928151e23627bc5bcecdeeff94a5683c7a7f6348
Moses_Ndungu_G_ <moisendungu@gmail.com>
2025-05-06 19:58:24 +0300

    Fixed status bar and navigation bar colors

--- app/src/main/java/com/ravemaster/ravechat/activities/SplashLaunchActivity.java
+++ app/src/main/java/com/ravemaster/ravechat/activities/SplashLaunchActivity.java
@@ -4,12 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.splashscreen.SplashScreen;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.ravemaster.ravechat.R;
 import com.ravemaster.ravechat.utilities.Constants;
